53d6792b898cd1d1e27aa34c3ba6d431720cab35
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-04-11 19:49:34 +0400

    Change card corners

--- spaceXApp/spaceXApp/HomePage/LeftWaveCardShape.swift
+++ spaceXApp/spaceXApp/HomePage/LeftWaveCardShape.swift
@@ -8,8 +8,6 @@
 
 import SwiftUI
 
-
-
 struct CardWithBottomRightCutout: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -20,6 +18,7 @@ struct CardWithBottomRightCutout: Shape {
 
         let cutoutWidth: CGFloat = 90
         let cutoutHeight: CGFloat = 70
+        let cutoutRadius: CGFloat = 20
 
         path.move(to: CGPoint(x: cornerRadius, y: 0))
 
@@ -27,12 +26,20 @@ struct CardWithBottomRightCutout: Shape {
         path.addQuadCurve(to: CGPoint(x: w, y: cornerRadius),
                           control: CGPoint(x: w, y: 0))
 
-        path.addLine(to: CGPoint(x: w, y: h - cutoutHeight))
 
-        path.addLine(to: CGPoint(x: w - cutoutWidth, y: h - cutoutHeight))
-        path.addLine(to: CGPoint(x: w - cutoutWidth, y: h))
+        path.addLine(to: CGPoint(x: w, y: h - cutoutHeight - cutoutRadius))
+        path.addQuadCurve(to: CGPoint(x: w - cutoutRadius, y: h - cutoutHeight),
+                          control: CGPoint(x: w, y: h - cutoutHeight))
+
+        path.addLine(to: CGPoint(x: w - cutoutWidth + cutoutRadius, y: h - cutoutHeight))
+        path.addQuadCurve(to: CGPoint(x: w - cutoutWidth, y: h - cutoutHeight + cutoutRadius),
+                          control: CGPoint(x: w - cutoutWidth, y: h - cutoutHeight))
+
+        path.addLine(to: CGPoint(x: w - cutoutWidth, y: h - cornerRadius))
+        path.addQuadCurve(to: CGPoint(x: w - cutoutWidth - cutoutRadius, y: h),
+                          control: CGPoint(x: w - cutoutWidth, y: h))
+
         path.addLine(to: CGPoint(x: cornerRadius, y: h))
-
         path.addQuadCurve(to: CGPoint(x: 0, y: h - cornerRadius),
                           control: CGPoint(x: 0, y: h))
 
@@ -44,6 +51,7 @@ struct CardWithBottomRightCutout: Shape {
         return path
     }
 }
+
 
 
 
